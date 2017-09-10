188eb71d44724028838ba84cfdb9761800c96cea
HILL\greg <grzegorz.janoszek@gmail.com>
2017-09-10 23:21:38 +0200

    Json properties

--- BackendMobileQueueService/DataObjects/JsonObject/PostValue.cs
+++ BackendMobileQueueService/DataObjects/JsonObject/PostValue.cs
@@ -1,11 +1,18 @@
-﻿namespace BackendMobileQueueService.DataObjects.JsonObject
+﻿using Newtonsoft.Json;
+
+namespace BackendMobileQueueService.DataObjects.JsonObject
 {
     public class PostValue
     {
+        [JsonProperty]
         public int StatusQueue { get; set; }
+        [JsonProperty]
         public int ExpectedTime { get; set; }
+        [JsonProperty]
         public bool Tendency { get; set; }
+        [JsonProperty]
         public bool Result { get; set; }
+        [JsonProperty]
         public string Info {get;set;}
     }
 }
