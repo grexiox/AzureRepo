f7254c6f8df673fdb17009185c532fadcee12c79
HILL\greg <grzegorz.janoszek@gmail.com>
2017-09-16 15:05:53 +0200

    config repair

--- BackendMobileQueueService/Models/BackendMobileQueueContext.cs
+++ BackendMobileQueueService/Models/BackendMobileQueueContext.cs
@@ -16,7 +16,7 @@ namespace BackendMobileQueueService.Models
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
 
-        private const string connectionStringName = "Name=MS_DB_CONNECTIONSTRING";
+        private const string connectionStringName = "Name=MS_TableConnectionString";
 
         public BackendMobileQueueContext() : base(connectionStringName)
         {
